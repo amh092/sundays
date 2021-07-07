ce615037164ab3981f65bbf13769f772b2f6c2f9
amh092 <ahmedalhazza@Ahmeds-MacBook-Pro.local>
2021-07-08 01:35:12 +0300

    just commitin g

--- main.js
+++ main.js
@@ -2,9 +2,11 @@
 var burger = document.getElementById("burger");
 var headerNav = document.querySelector("header nav ul");
 var triangle = document.querySelector(".triangle");
+var arrowDown = document.querySelector(".arrow-down");
 
 burger.addEventListener("click",function(){
     headerNav.classList.toggle("enabled")
-    triangle.classList.toggle("enabled")
+    triangle.classList.toggle("enabled-tri")
+   
     
 })
