ccb9b4f2b87d62a65933ce1b919b573829b31dc1
amh092 <ahmedalhazza@Ahmeds-MacBook-Pro.local>
2021-07-08 19:06:31 +0300

    mobile day  2

--- main.js
+++ main.js
@@ -3,10 +3,16 @@ var burger = document.getElementById("burger");
 var headerNav = document.querySelector("header nav ul");
 var triangle = document.querySelector(".triangle");
 var arrowDown = document.querySelector(".arrow-down");
+var arrowDown2 = document.querySelector(".arrow-down h1");
+var arrowDown3 = document.querySelector(".arrow-down img");
+
 
 burger.addEventListener("click",function(){
     headerNav.classList.toggle("enabled")
     triangle.classList.toggle("enabled-tri")
+    arrowDown.classList.toggle("hide-it")
+    arrowDown2.classList.toggle("hide-it")
+    arrowDown3.classList.toggle("hide-it")
    
     
 })
